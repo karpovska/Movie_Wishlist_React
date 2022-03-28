3d059cb7aefaaa41bb081e2949a2d23f6f3b3899
Oksana Karpova <50658775+karpovska@users.noreply.github.com>
2022-03-28 19:57:23 +0200

    Api call added

--- src/components/Add.js
+++ src/components/Add.js
@@ -6,8 +6,17 @@ export const Add = () => {
     event.preventDefault();
 
     setQuery(event.target.value);
-  };
 
+    fetch(
+      `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&t=${event.target.value}`
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  //http://www.omdbapi.com/?apikey=[yourkey]&
+  //http://www.omdbapi.com/?t=Iron+man
   return (
     <div className="add-page">
       <div className="container">
