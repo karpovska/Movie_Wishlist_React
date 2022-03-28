530a21f6a93c4f3f4758f25a2bf63aff6f0fb81a
Oksana Karpova <50658775+karpovska@users.noreply.github.com>
2022-03-28 14:33:01 +0200

    Input field added to the search

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Add = () => {
+  const [query, setQuery] = useState("");
+  const onChange = (event) => {
+    event.preventDefault();
+
+    setQuery(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>Add Page</h1>
+    <div className="add-page">
+      <div className="container">
+        <div className="add-content">
+          <div className="input-wrapper">
+            <input
+              type="text"
+              placeholder="Search for a movie"
+              value={query}
+              onChange={onChange}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
