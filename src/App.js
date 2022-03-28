b2db907907c354bd787271231dd6fd98416b6a89
Oksana Karpova <50658775+karpovska@users.noreply.github.com>
2022-03-28 14:18:06 +0200

    Routing bug fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Wishlist } from "./components/Wishlist";
 import { Add } from "./components/Add";
@@ -10,15 +10,11 @@ function App() {
     <Router>
       <Header />
 
-      <Switch>
-        <Route exact path="/">
-          <Wishlist />
-        </Route>
+      <Routes>
+        <Route exact path="/" element={<Wishlist />} />
 
-        <Route path="/add">
-          <Add />
-        </Route>
-      </Switch>
+        <Route path="/add" element={<Add />} />
+      </Routes>
     </Router>
   );
 }
