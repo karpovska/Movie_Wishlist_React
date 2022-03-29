1debff69e4696b4141118a8b1a003ca411d4fb96
Oksana Karpova <50658775+karpovska@users.noreply.github.com>
2022-03-29 02:42:40 +0200

    Add title, year and add to wishlist button to movie card

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -10,6 +10,17 @@ export const ResultCard = ({ movie }) => {
           <div className="filler-poster"></div>
         )}
       </div>
+
+      <div className="info">
+        <div className="header">
+          <h3 className="title">{movie.Title}</h3>
+          <h4 className="release-date">{movie.Year ? movie.Year : "-"}</h4>
+        </div>
+
+        <div className="constrols">
+          <button className="btn">Add to Wishlist</button>
+        </div>
+      </div>
     </div>
   );
 };
