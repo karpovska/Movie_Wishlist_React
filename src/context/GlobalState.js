bf8df60d624527ce580eddf311ee7619d0cabff6
Oksana Karpova <50658775+karpovska@users.noreply.github.com>
2022-03-29 12:20:25 +0200

    Add reducer and context to manage wishlist

---
+++ src/context/GlobalState.js
@@ -0,0 +1,26 @@
+import React, { createContext, useReducer, useEffect } from "react";
+import { AppReducer } from "./AppReducer";
+
+// create initial state
+const initialState = {
+  wishlist: []
+};
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// create provider components
+export const GlobalProvider = (props) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // create actions
+  const addMovieToWishlist = (movie) => {
+    dispatch({ type: "ADD_MOVIE_TO_WISHLIST", payload: movie });
+  };
+
+  return (
+    <GlobalContext.Provider value={{ wishlist: state.wishlist }}>
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
